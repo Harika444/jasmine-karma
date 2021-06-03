13d750645602e98505f40a4cca21430a77941bde
Harika444 <47811739+Harika444@users.noreply.github.com>
2021-06-03 14:58:34 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/0.13/config/configuration-file.html
-const os = require('os');
-const chromeHeadlessSupported = os.platform() !== 'win32' || Number((os.release().match(/^(\d+)/) || ['0', '0'])[1]) >= 10;
 
 module.exports = function (config) {
   config.set({
@@ -9,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular/cli/plugins/karma')
@@ -29,16 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browserNoActivityTimeout: 30000,
-    browsers: [
-      chromeHeadlessSupported ? 'ChromeHeadless' : 'Chrome'
-    ],
-    customLaunchers: {
-      ChromeHeadless: {
-          base: 'Chrome',
-          flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
-      }
-    },
-    singleRun: false
+    browsers: ['PhantomJS'],
+    singleRun: true
   });
 };
